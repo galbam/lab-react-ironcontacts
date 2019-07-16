b38a14e8aa3d425421bd2dbfb6275d4e218f1635
Gonzalo <gonzalo.alba@live.com>
2019-07-16 23:07:17 +0200

    Missing delete

---
+++ starter-code/src/components/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {   
+
+     render() {
+
+          return (<tr>
+               <td><img src={this.props.contact.pictureUrl} style={{ width: "20vh", display: "block", marginLeft: "auto", marginRight: "auto" }} alt="x" /></td>
+               <td style={{textAlign: "center"}}><h2>{this.props.contact.name}</h2></td>
+               <td style={{textAlign: "center"}}><h2>{this.props.contact.popularity}</h2></td>
+               <td style={{textAlign: "center"}}><button style={{ fontSize: "1rem" }}>Delete</button></td>
+          </tr>);
+     }
+}
+
+export default Contact;
+
+///deleteContact={this.deleteContact}
+//onClick={this.props.deleteContact.bind(this.props.key)}
